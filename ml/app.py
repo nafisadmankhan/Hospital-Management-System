6ab1ace73fcfb6c0056c41e57e61117fb2b1908c
Nafi Sadman Khan <sadmannafi789@gmail.com>
2026-05-11 11:08:44 +0600

    Integrated Flask. Flask API is working

--- ml/app.py
+++ ml/app.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/python/test')
-def hello():
+@app.route('/api/ml/test')
+def api():
     return jsonify({"message": "Hello from Flask inside Docker!"})
 
+@app.route('/')
+def hello():
+    return "The Flask server is definitely working!"
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=True)
